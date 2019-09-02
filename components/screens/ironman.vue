cf5f200ab9453702306221f3f7d57937a6786179
Mohamed Nabeel Moidunni <kpnamo@gmail.com>
2019-09-02 14:09:39 +0530

    Adding the screens for Iron Man & Thanos

---
+++ components/screens/ironman.vue
@@ -0,0 +1,24 @@
+<template functional>
+    <scroll-view>
+        <view class="member">
+            <image
+                :style="{width: 420, height: 350}"
+                :source="{uri: 'https://nerdist.com/wp-content/uploads/2019/03/IM-header-1200x676.jpg'}"
+            />
+            <text class="member_title">Tony Stark aka Iron Man</text>
+            <text class="member_details">
+                Iron Man is a fictional superhero appearing in American comic books published by Marvel Comics. The character was co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby. The character made his first appearance in Tales of Suspense #39 (cover dated March 1963), and received his own title in Iron Man #1 (May 1968).
+            </text>
+        </view>
+    </scroll-view>
+</template>
+
+<style>
+.member_title {
+    font-size:30;
+    padding:15px;
+}
+.member_details {
+    padding: 15px;
+}
+</style>
---
+++ components/screens/thanos.vue
@@ -0,0 +1,24 @@
+<template functional>
+    <scroll-view>
+        <view class="member">
+            <image
+                :style="{width: 420, height: 350}"
+                :source="{uri: 'https://wallpapersmug.com/large/5a3fd5/thanos-avengers-infinity-4k.jpg'}"
+            />
+            <text class="member_title">Thanos</text>
+            <text class="member_details">
+                Thanos is a fictional supervillain appearing in American comic books published by Marvel Comics. The character, created by writer/artist Jim Starlin, first appeared in The Invincible Iron Man #55 (cover dated February 1973). Thanos is one of the most powerful villains in the Marvel Universe and has clashed with many heroes including the Avengers, the Guardians of the Galaxy, the Fantastic Four, and the X-Men.
+            </text>
+        </view>
+    </scroll-view>
+</template>
+
+<style>
+.member_title {
+    font-size:30;
+    padding:15px;
+}
+.member_details {
+    padding: 15px;
+}
+</style>
