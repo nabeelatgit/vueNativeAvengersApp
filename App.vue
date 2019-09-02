69f22cf28d78e674f94a4bf472540250e4b37b7c
Mohamed Nabeel Moidunni <kpnamo@gmail.com>
2019-09-02 13:59:29 +0530

    Adding the basic files

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <app-navigation></app-navigation>
+</template>
+
+<script> 
+  import { StackNavigator } from "vue-native-router";
+  import Home from "./components/index";
+  import thanos from "./components/screens/thanos";
+  import ironman from "./components/screens/ironman";
+  const AppNavigation = StackNavigator(
+    {
+      Home: Home,
+      thanos: thanos,
+      ironman: ironman
+    },
+    {
+      initialRouteName: 'Home',
+    }
+  );
+  export default {
+    components: { AppNavigation }
+  };
+</script>
+ 
+<style>
+</style>
